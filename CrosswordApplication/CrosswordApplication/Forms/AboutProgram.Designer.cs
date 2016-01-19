f7f6eed381c068f97306330779d1a645f41b033c
lena15n <ln15ln@rambler.ru>
2016-01-19 13:52:59 +0400

    Edit "About program"

--- CrosswordApplication/CrosswordApplication/Forms/AboutProgram.Designer.cs
+++ CrosswordApplication/CrosswordApplication/Forms/AboutProgram.Designer.cs
@@ -35,7 +35,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -52,7 +52,7 @@
             // okButton
             // 
             this.okButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.okButton.Location = new System.Drawing.Point(482, 132);
+            this.okButton.Location = new System.Drawing.Point(280, 146);
             this.okButton.Name = "okButton";
             this.okButton.Size = new System.Drawing.Size(75, 23);
             this.okButton.TabIndex = 1;
@@ -64,7 +64,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(159, 12);
+            this.label1.Location = new System.Drawing.Point(158, 80);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(73, 17);
             this.label1.TabIndex = 2;
@@ -74,7 +74,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(158, 37);
+            this.label2.Location = new System.Drawing.Point(157, 105);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(134, 17);
             this.label2.TabIndex = 3;
@@ -84,7 +84,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(324, 37);
+            this.label3.Location = new System.Drawing.Point(323, 105);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(237, 17);
             this.label3.TabIndex = 4;
@@ -94,24 +94,23 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(251, 12);
+            this.label4.Location = new System.Drawing.Point(250, 80);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(310, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Карпова Е.Д., Назарова Е.А., Пономарев Е.А.";
             // 
-            // label5
+            // label6
             // 
-            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(158, 59);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(406, 70);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "Автоматизированная система составления и разгадывания классического кроссворда.  " +
-    "\r\nСистема позволяет разгадывать и составлять кроссворды вручную или автоматическ" +
-    "и.";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(158, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(443, 51);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Курсовой проект по теме \"Автоматизированная система\r\nсоставления и разгадывания к" +
+    "лассического кроссворда\" \r\nпо дисциплине \"Программная инженерия\"";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // AboutProgram
             // 
@@ -119,8 +118,8 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(569, 167);
-            this.Controls.Add(this.label5);
+            this.ClientSize = new System.Drawing.Size(606, 181);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -149,6 +148,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
